9c6ec9f364c1af84c573bb54166a86cea2d2bd48
justtrial8792 <149057676+justtrial8792@users.noreply.github.com>
2023-12-24 19:31:57 +0600

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,8 +16,8 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002020577692'))
-    PORT = int(getenv('PORT', '443'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '104.42.168.149'))
+    PORT = int(getenv('PORT', '8181'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '20.125.124.87'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5734654177").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -29,7 +29,7 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', '104.42.168.149:443')) if not ON_HEROKU or getenv('FQDN', '104.42.168.149:443') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '20.125.124.87:8181')) if not ON_HEROKU or getenv('FQDN', '20.125.124.87:8181') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
